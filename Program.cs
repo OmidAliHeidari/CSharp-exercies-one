ce1b18d34a178ce54ee73f7b649b3a8dad5da80d
Omid Ali Heidari <omidaliheidari@gmail.com>
2023-05-17 06:44:50 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// iki sayının toplamını veren uygulama
+Console.Write("Please enter your first number: ");
+int numOne = Convert.ToInt32(Console.ReadLine());
+Console.Write("Please enter your second number: ");
+int numTwo = Convert.ToInt32(Console.ReadLine());
+int sum = numOne + numTwo;
+Console.WriteLine("Sum of your numbers: " + sum);
